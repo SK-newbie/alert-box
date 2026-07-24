a902599e14463ba1b2bde96cfef89e7c61b27f4e
Oseagwina Excellence Oseobulu <156667746+ExcellenceOseagwina@users.noreply.github.com>
2026-07-24 10:05:28 +0100

    cc

--- alert.js
+++ alert.js
@@ -1,19 +1,19 @@
-const buttonOne = document.querySelector('.btn-1');
-const buttonTwo = document.querySelector('.btn-2');
-const container = document.querySelector('.container');
-const buttonThree = document.querySelector('.btn-3');
+const buttonOne = document.querySelector(".btn-1");
+const buttonTwo = document.querySelector(".btn-2");
+const container = document.querySelector(".container");
+const buttonThree = document.querySelector(".btn-3");
 
-    function setAlert() {
-        alert('I love Javascript');
-    }
-    function changeBgcolor(){
-        container.classList.add('containerChange');
-    }
+function setAlert() {
+  alert("I love Javascript");
+}
+function changeBgcolor() {
+  container.classList.add("containerChange");
+}
 
-    function tranformButton(){
-        container.classList.remove('containerChange');
-    }
+function tranformButton() {
+  container.classList.remove("containerChange");
+}
 
-buttonOne.addEventListener('click', setAlert);
-buttonTwo.addEventListener('click', changeBgcolor);
-buttonThree.addEventListener('click', tranformButton)
+buttonOne.addEventListener("click", setAlert);
+buttonTwo.addEventListener("click", changeBgcolor);
+buttonThree.addEventListener("click", tranformButton);
